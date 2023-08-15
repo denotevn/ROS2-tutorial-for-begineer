fbaa3f880823402e0267aac4e15ce1551545d35c
Tuan Dinh <dinhngoctuan2131997@gmail.com>
2023-08-15 19:57:20 +0300

    declare parameter in ROS2

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ROS2-tutorial-for-begineer
+
+## How to create packages in ROS2
+
+## How to create node in ros2
+
+## How to create custom msg and srv in ros2
--- ex02/my_py_ex2_pkg/my_py_ex2_pkg/number_publisher.py
+++ ex02/my_py_ex2_pkg/my_py_ex2_pkg/number_publisher.py
@@ -7,9 +7,19 @@ from example_interfaces.msg import Int64
 class NumberPublisherNode(Node):
     def __init__(self):
         super().__init__("number_publisher") # modify name
-        self.number_ = 2
+        # declare new param
+        # need to add type of param (it is very important)
+        # self.declare_parameter("test123") # no set value
+        # self.declare_parameter("another_param", type=String) 
+        # lam viec voi parameter cua minh
+        self.declare_parameter("number_to_publish", 2)
+        self.declare_parameter("publish_frequency", 1.0)
+
+
+        self.number_ = self.get_parameter("number_to_publish").value  # 2
+        self.publish_frequency_ = self.get_parameter("publish_frequency").value
         self.number_publisher_ = self.create_publisher(Int64, 'number', 10) # number - topic
-        self.number_timer_ = self.create_timer(1.0, self.publish_number)
+        self.number_timer_ = self.create_timer(1.0 / self.publish_frequency_, self.publish_number)
         self.get_logger().info("Number publisher has been started.")
 
     def publish_number(self):
